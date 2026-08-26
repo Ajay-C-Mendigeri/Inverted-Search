cae8b052a3d1f4d3673c92e629405ca374d684bf
Ajay C M <ajaymendigeri@gmail.com>
2026-08-26 12:17:25 +0000

    project

--- save_database.c
+++ save_database.c
@@ -1,7 +1,42 @@
 #include "header.h"
 
-void save_database(hash_t *hash_table)
-{
-  
+void save_database(hash_t *hash_table){
+  char file[20];
+  printf("Enter the file name: ");
+  scanf("%s",file);
+
+ 
+  char *ptr=strrchr(file,'.');
+  if(ptr==NULL || strcmp(ptr,".txt")!=0){
+    printf("Not validate file\n");
+    return;
+  }
+
+  FILE *fp=fopen(file,"w");
+  if(fp==NULL){
+    printf("File opens Fails\n");
+    return;
+  }
+
+  for(int i=0; i<28;i++){
+    if(hash_table[i].m_link==NULL){
+        continue;
+    }
+
+    m_node *temp_m=hash_table[i].m_link;
+    while(temp_m!=NULL){
+        fprintf(fp,"\n#;%d;%s;%d;",i,temp_m->words,temp_m->f_count);
+        s_node *temp_s=temp_m->s_link;
+        while(temp_s!=NULL){
+            fprintf(fp,"%s;%d;#\n",temp_s->filename,temp_s->w_count);
+            temp_s=temp_s->s_link;
+        }
+       temp_m=temp_m->m_link;
+
+    }
+    
+}
+fclose(fp);
+
 
 }
--- update_database.c
+++ update_database.c
@@ -2,5 +2,16 @@
 
 void update_database(hash_t *hash_table)
 {
-    //printf("Update database\n");
+    char  file[20];
+    printf("Enter the file name: ");
+    scanf("%s", file);
+
+    char *ptr = strrchr(file, '.'); 
+    if (ptr == NULL || strcmp(ptr, ".txt") != 0) {
+        printf("Not validate file\n");
+        return;
+    }
+
+    FILE *fp = fopen(file, "r");
+
 }
